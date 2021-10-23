ab0aca069c25978e4c4ddc76c78c677fbe2ba8cd
Dinesh Bhattarai <dineshdb@dbhattarai.info.np>
2021-10-23 21:08:47 +0545

    feat/lib/k8s: helmInstall, kaf and kdf functions

--- lib/k8s.ts
+++ lib/k8s.ts
@@ -5,7 +5,18 @@ export function repo(name: string, url: string | URL) {
 }
 
 export function update(repo?: string) {
-  return sh(`helm repo update ${repo ?? ''}`);
+  return sh(`helm repo update ${repo ?? ""}`);
+}
+
+export function helmInstall(
+  name: string,
+  repo: string,
+  ...values: Array<string>
+) {
+  return sh(
+    `helm upgrade -i --create-namespace ${name} ${repo} ` +
+      (values.length > 0 ? " -f " + values.join(" -f ") : ""),
+  );
 }
 
 export function k(cmd: string) {
@@ -13,4 +24,6 @@ export function k(cmd: string) {
 }
 
 export const ka = (cmd: string) => k("apply " + cmd);
-export const kaf = (cmd: string) => k("apply -f " + cmd);
+export const kad = (...cmd: Array<string>) =>
+  k("delete -f " + cmd.join(" -f "));
+export const kaf = (...cmd: Array<string>) => k("apply -f " + cmd.join(" -f "));
